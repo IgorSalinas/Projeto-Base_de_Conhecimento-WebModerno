9b7fbf3731ece588f1bd5006ed3228f7b7174aab
Igor <igor_salinas@outlook.com>
2020-02-22 16:13:28 -0300

    Implementando Knex

---
+++ backend/knexfile default.js
@@ -0,0 +1,18 @@
+// Update with your config settings.
+
+module.exports = {
+    client: 'postgresql',
+    connection: {
+      database: '',
+      user:     '',
+      password: ''
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+
+};
