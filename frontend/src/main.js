0cb936142e71d9a2de271ccf5d3e715b3689986e
Igor <igor_salinas@outlook.com>
2020-03-06 23:23:06 -0300

    Cadastro e remoção de usuário

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,13 +4,14 @@ import Vue from 'vue'
 import App from './App'
 
 import './config/bootstrap'
+import './config/msgs'
 import store from './config/store'
 import router from  './config/router'
 
 Vue.config.productionTip = false
 
 // TEMPORARIO!
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Iklnb3IgU2FsaW5hcyIsImVtYWlsIjoiaWdvckBzYWxpbmFzLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE1ODMyODM1OTAsImV4cCI6MTU4MzU0Mjc5MH0.Ev7siMMGmdxc6S-_o8M2pHM30YEJrTKlQEIcUlLyLUU'
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Iklnb3IgU2FsaW5hcyIsImVtYWlsIjoiaWdvckBzYWxpbmFzLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE1ODM1NDAxODQsImV4cCI6MTU4Mzc5OTM4NH0.mhdUZKUPJJDA8I3ChpX4LqMjYGVOBOqbXhY_Evmu6kg'
 
 new Vue({
   store,
